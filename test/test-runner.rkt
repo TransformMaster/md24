786dec4fd283f873dfe54e882a756370e8a5e006
TransformMaster <52907488+TransformMaster@users.noreply.github.com>
2022-04-22 14:40:53 -0400

    Update test-runner.rkt

--- test/test-runner.rkt
+++ test/test-runner.rkt
@@ -153,7 +153,7 @@
                 1)
   (check-equal? (run '(define (f x) x)
                      '(f 1 2 3))
-                1)
+                2)
   
   ;; Iniquity tests
   (check-equal? (run
